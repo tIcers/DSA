4ed02c04ad37834b6daf62bbad633923aff24d23
Atsuki Uchida <csatsfapsfu2930@gmail.com>
2022-07-24 16:54:13 +0900

    solved arr prob

--- arrays.py
+++ arrays.py
@@ -110,3 +110,28 @@ def pair_sum(arr,k):
 			ouput.add((min(num, target), max(num,target)))
 	
 	return len(ouput)
+
+
+"""
+Consider an array of non-negative integers. A second array is formed by shuffling the elements of the first array and deleting a random element. Given these two arrays, find which element is missing in the second array.
+
+the first array is shuffled and the number 5 is removed to construct the second array.
+
+Input:
+
+finder([1,2,3,4,5,6,7],[3,7,2,1,4,6])
+Output:
+
+5 is the missing number"""
+
+def finder(arr1,arr2):
+	arr1 = arr1.sort()
+	arr2 = arr2.sort()
+
+	for num1,num2 in zip(arr1,arr2):
+		if num1 != num2:
+			return num1
+
+	return arr1[-1]
+
+
