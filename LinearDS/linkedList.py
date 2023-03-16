9f432e06ee0d84c1b821974290e69c4ebcac6890
atsuki <csatsfapsfu2930@gmail.com>
2023-03-16 00:41:48 -0700

    implemented insert and stringfiy_list

--- LinearDS/linkedList.py
+++ LinearDS/linkedList.py
@@ -17,8 +17,24 @@ my_node = Node(44)
 
 print(my_node.get_value())
 
+
 class LinkedList:
-    def __init__(self, value = None):
+    def __init__(self, value=None):
         self.head_node = Node(value)
+
     def get_head_node(self):
         return self.head_node
+
+    def insert_beginning(self, new_value):
+        new_node= Node(new_value)
+        new_node.set_next_node(self.head_node)
+        self.head_node = new_nod
+
+    def stringify_list(self):
+        string_list = ""
+        current_node = self.get_head_node()
+        while current_node:
+            if current_node.get_value() != None:
+                string_list += str(current_node.get_value()) + "\n"
+                current_node = current_node.get_next_node()
+        return string_list
