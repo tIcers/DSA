6691a68f4598b7b346ad6e11cf20f1c3d72fdc19
atsuki <csatsfapsfu2930@gmail.com>
2023-03-16 13:30:23 -0700

    fixed typo

--- LinearDS/linkedList.py
+++ LinearDS/linkedList.py
@@ -49,7 +49,7 @@ print(ll.stringify_list()
         if current_node.get_value() == value_to_remove:
             self.head_node = current_node.get_next_node()
         else:
-            while current_nodeL
+            while current_node:
                 next_node = current_node.get_next_node()
                 if next_node.get_value() == value_to_remove:
                     current_node.set_next_node(next_node.get_next_node())
