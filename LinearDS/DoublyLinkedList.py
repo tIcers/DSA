6c08ef5593b7718859707e03cd4b95424a16d74e
atsuki <csatsfapsfu2930@gmail.com>
2023-04-19 16:34:43 -0700

    added remove_tail

--- LinearDS/DoublyLinkedList.py
+++ LinearDS/DoublyLinkedList.py
@@ -1,70 +1,85 @@
 class Node:
-    def __init__(self, value, next_node=None, prev_node=None, head_node=None, tail_node=None):
-        self.value = value
-        self.next_node = next_node
-        self.prev_node = prev_node
-        self.head_node = head_node
-        self.tail_node = tail_node
+  def __init__(self, value, next_node=None, prev_node=None):
+    self.value = value
+    self.next_node = next_node
+    self.prev_node = prev_node
+    
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+    
+  def get_next_node(self):
+    return self.next_node
 
-    def set_next_node(self, next_node):
-        self.next_node = next_node
-
-    def get_next_node(self):
-        return self.next_node
-
-    def set_prev_node(self, prev_node):
-        self.prev_node = prev_node
-
-    def get_prev_node(self):
-        return self.prev_node
-
-    def get_value(self):
-        return self.value
-
+  def set_prev_node(self, prev_node):
+    self.prev_node = prev_node
+    
+  def get_prev_node(self):
+    return self.prev_node
+  
+  def get_value(self):
+    return self.value
+    
 
 class DoublyLinkedList:
-    def __init__(self, head_node=None, tail_node=None):
-        self.head_node = head_node
-        self.tail_node = tail_node
+  def __init__(self):
+    self.head_node = None
+    self.tail_node = None
+  
+  def add_to_head(self, new_value):
+    new_head = Node(new_value)
+    current_head = self.head_node
 
-    def add_to_head(self, new_value):
-        new_head = Node(new_value)
-        current_head = self.head_node
+    if current_head != None:
+      current_head.set_prev_node(new_head)
+      new_head.set_next_node(current_head)
 
-        if current_head != None:
-            current_head.set_prev_node(new_head)
-            new_head.set_next_node(current_head)
-        else:
-            self.tail_node == None
-            self.tail_node = new_head
-        self.head_node = new_head 
-    
-    def add_to_tail(self, new_value):
-      new_tail= Node(new_value)
-      current_tail = self.tail_node
+    self.head_node = new_head
 
-      if current_tail != None:
-        current_tail.set_next_node(new_tail)
-        new_tail.set_prev_node(current_tail)
-      
-      self.tail_node = new_tail
+    if self.tail_node == None:
+      self.tail_node = new_head
 
-      if self.head_node == None:
-        self.head_node = new_tail
-      
-    def remove_head(self):
-      removed_head = self.head_node
-      
-      
-      if removed_head == None:
-        return None
-     
+  def add_to_tail(self, new_value):
+    new_tail = Node(new_value)
+    current_tail = self.tail_node
 
-      self.head_node = removed_head.get_next_node()
-      
-      if removed_head != None:
-        self.head_node.set_prev_node(None) 
+    if current_tail != None:
+      current_tail.set_next_node(new_tail)
+      new_tail.set_prev_node(current_tail)
 
-      if removed_head == self.tail_node:
-        self.remove_tail()
-      return removed_head.get_value()
+    self.tail_node = new_tail
+
+    if self.head_node == None:
+      self.head_node = new_tail
+
+  def remove_head(self):
+    removed_head = self.head_node
+
+    if removed_head == None:
+      return None
+
+    self.head_node = removed_head.get_next_node()
+
+    if self.head_node != None:
+      self.head_node.set_prev_node(None)
+
+    if removed_head == self.tail_node:
+      self.remove_tail()
+
+    return removed_head.get_value()
+
+  def remove_tail(self):
+    removed_tail = self.tail_node
+
+    if removed_tail == None:
+      return None
+
+    self.tail_node = removed_tail.get_prev_node()
+
+    if self.tail_node != None:
+      self.tail_node.set_next_node(None)
+
+    if removed_tail == self.head_node:
+      self.remove_head()
+
+    return removed_tail.get_value()
+
