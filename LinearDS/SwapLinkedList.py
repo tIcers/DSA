98c40d370538a9cbab69e09c565890e7fe0e0673
atsuki <csatsfapsfu2930@gmail.com>
2023-06-05 12:20:59 -0700

    finished implementing swapLinkedList

--- LinearDS/SwapLinkedList.py
+++ LinearDS/SwapLinkedList.py
@@ -6,20 +6,56 @@
 # as we have found the correct node. if there is no match we update node1_prev to be node1 and move node1 to its next node
 
 
+import Node
+import LinkedList
+
 def swap_nodes(input_list, val1, val2):
-    node1 = input_list.head_node
-    node2 = input_list.head_node
-    node1_prev = None
-    node2_prev = None
+  print(f'Swapping {val1} with {val2}')
 
-    while node1 is not None:
-        if node1.get_value() = val1:
-            break
-        node1_prev = node1
-        node1 = node1.get_next_value()
+  node1_prev = None
+  node2_prev = None
+  node1 = input_list.head_node
+  node2 = input_list.head_node
 
-    while node2 is not None:
-        if node2.get_value() = val2:
-            break
-        node2_prev = node2
-        node2 = node2.get_next_value()
+  if val1 == val2:
+    print("Elements are the same - no swap needed")
+    return
+
+  while node1 is not None:
+    if node1.get_value() == val1:
+      break
+    node1_prev = node1
+    node1 = node1.get_next_node()
+
+  while node2 is not None:
+    if node2.get_value() == val2:
+      break
+    node2_prev = node2
+    node2 = node2.get_next_node()
+
+  if (node1 is None or node2 is None):
+    print("Swap not possible - one or more element is not in the list")
+    return
+
+  if node1_prev is None:
+    input_list.head_node = node2
+  else:
+    node1_prev.set_next_node(node2)
+
+  if node2_prev is None:
+    input_list.head_node = node1
+  else:
+    node2_prev.set_next_node(node1)
+
+  temp = node1.get_next_node()
+  node1.set_next_node(node2.get_next_node())
+  node2.set_next_node(temp)
+
+
+ll = LinkedList.LinkedList()
+for i in range(10):
+  ll.insert_beginning(i)
+
+print(ll.stringify_list())
+swap_nodes(ll, 9, 5)
+print(ll.stringify_list())
