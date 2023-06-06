4b2a292d637f3af4f4cb1193ccd889c2c514f80d
atsuki <csatsfapsfu2930@gmail.com>
2023-06-06 13:19:34 -0700

    added try and exception

--- LinearDS/LinearSearch/script.py
+++ LinearDS/LinearSearch/script.py
@@ -1,12 +1,31 @@
 
+# def linear_search(search_list, target_value):
+#   for idx in range(len(search_list)):
+#     print(search_list[idx])
+#     if search_list[idx] == target_value:
+#       return idx
+#   raise ValueError(f"{target_value} not in list")
+#
+#
+#
+# values = [54, 22, 46, 99]
+# print(linear_search(values, 22))
+
+
+number_list = [ 10, 14, 19, 26, 27, 31, 33, 35, 42, 44]
+target_number = 33
+
 def linear_search(search_list, target_value):
   for idx in range(len(search_list)):
-    print(search_list[idx])
     if search_list[idx] == target_value:
       return idx
-  raise ValueError(f"{target_value} not in list")
+  raise ValueError("{0} not in list".format(target_value))
 
 
 
-values = [54, 22, 46, 99]
-print(linear_search(values, 22))
+try:
+    result = linear_search(number_list, target_number)
+    print(result)
+    linear_search(number_list, 100)
+except ValueError as error_message:
+  print("{0}".format(error_message))
