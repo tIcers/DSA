2f1bfaa88d9d2c6c7812ae51466a81b6a25520ff
atsuki <csatsfapsfu2930@gmail.com>
2023-06-06 15:15:15 -0700

    implemented another if statement to check right and left

--- LinearDS/BinarySearch/binaryRecursive.py
+++ LinearDS/BinarySearch/binaryRecursive.py
@@ -6,10 +6,20 @@ def binary_search(sorted_list, target):
     mid_val = sorted_list[mid_idx] 
     if mid_val == target:
         return mid_idx
-    return mid_idx, mid_val
+    if mid_val > target:
+        left_half = sorted_list[:mid_idx] 
+        return binary_search(left_half, target)
+    if mid_val < target:
+        right_half = sorted_list[mid_idx + 1:]
+        result = binary_search(right_half, target)
+        if result == 'value not found':
+            return result
+        else:
+            return result + mid_idx + 1
 
 
 # For testing:
 sorted_values = [13, 14, 15, 16, 17]
 print(binary_search([], 42))
 print(binary_search(sorted_values, 42))
+print(binary_search(sorted_values, 17))
