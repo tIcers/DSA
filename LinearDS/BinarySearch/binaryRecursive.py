2ac6cc080f175b9efa1ccb9e9fee920c7ba8f426
atsuki <csatsfapsfu2930@gmail.com>
2023-06-06 15:29:48 -0700

    optimized previous code

--- LinearDS/BinarySearch/binaryRecursive.py
+++ LinearDS/BinarySearch/binaryRecursive.py
@@ -23,3 +23,26 @@ sorted_values = [13, 14, 15, 16, 17]
 print(binary_search([], 42))
 print(binary_search(sorted_values, 42))
 print(binary_search(sorted_values, 17))
+
+
+# realized above code is wasteful because it's making copy of N/2 elements where N i sthe length of the sorted list at each recursive call
+# so i willl implement code using pointer
+def binary_search(sorted_list, left_pointer, right_pointer, target):
+    if left_pointer >= right_pointer: # reached an empty sublist
+        return 'value not found'
+
+    mid_idx = (left_pointer + right_pointer) //2
+    mid_val = sorted_list[mid_idx]
+    if mid_val == target:
+        return mid_idx
+    if mid_val > target:
+        return binary_search(sorted_list, left_pointer, mid_idx, target)
+    if mid_val < target:
+        return binary_search(sorted_list, mid_idx + 1, right_pointer, target)
+values = [77, 80, 102, 123, 288, 300, 540]
+start_of_values = 0
+end_of_values = len(values)
+result = binary_search(values, start_of_values, end_of_values, 288)
+
+print("element {0} is located at index {1}".format(288, result))
+
