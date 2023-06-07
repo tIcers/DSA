7f8e93e26e18c541ebb2b7719d4008e5160e8a40
atsuki <csatsfapsfu2930@gmail.com>
2023-06-07 13:50:40 -0700

    gitsign testing

--- LinearDS/BinarySearch/script.py
+++ LinearDS/BinarySearch/script.py
@@ -1,11 +1,11 @@
 def sparse_search(data, search_val):
-    first = 0 
+    first = 0  
     last = len(data) - 1
 
     while first <= last:
-        mid = (first + last) // 2 
+        mid = (first + last) // 2
         if data[mid] == '':
-            left = mid - 1 
+            left = mid - 1
             right = mid + 1
             while True:
                 if left < first and right > last:
@@ -22,13 +22,20 @@ def sparse_search(data, search_val):
         if data[mid] == search_val:
             print("{0} found at position {1}".format(search_val, mid))
             return
+
+
+
+
+
+
+
+
         if search_val < data[mid]:
             last = mid - 1
         if search_val > data[mid]:
+
+
             first = mid + 1
     print("{0} is not in the dataset".format(search_val))
-
-
     print("Data: " + str(data))
     print("Search Value: " + str(search_val))
-  
