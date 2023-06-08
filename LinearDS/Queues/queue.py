c597b021edc69b0ab35ef4761898afae04623751
atsuki <csatsfapsfu2930@gmail.com>
2023-06-08 14:51:33 -0700

    initilized queue.py

--- LinearDS/Queues/queue.py
+++ LinearDS/Queues/queue.py
@@ -1,3 +1,12 @@
 from node import Node
 
+class Queue:
 
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def peek(self):
+        if self.head is None:
+            return None
+        return self.head.get_value()
