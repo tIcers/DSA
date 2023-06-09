e4f310eea98a0a2ec55427d8e65af1b5cc395418
atsuki <csatsfapsfu2930@gmail.com>
2023-06-08 17:11:31 -0700

    implemented dequeue method

--- LinearDS/Queues/queue.py
+++ LinearDS/Queues/queue.py
@@ -13,8 +13,7 @@ class Queue:
             item_to_add = Node(value)
             print("Adding " + str(item_to_add.get_value() + "to the queue!"))
             if self.is_empty():
-                self.head = item_to_add
-                self.tail = item_to_add
+                self.head = self.tail = item_to_add
             else:
                 self.tail.set_next_node(item_to_add)
                 self.tail = item_to_add
@@ -22,8 +21,18 @@ class Queue:
         else:
             print("no more room ")
 
-
-
+    def dequeue(self):
+        if self.get_size() > 0:
+            item_to_remove = self.head
+            print("Removing" + str(item_to_remove.get_value()) + "from the queue!")
+            if self.get_size() == 1:
+                self.head = self.tail = None
+            else:
+                self.head = self.head.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
+        else:
+            print("This queue is totally empty!")
 
     def peek(self):
         if self.is_empty():
@@ -44,6 +53,3 @@ class Queue:
 
     def is_empty(self):
         return self.size == 0
-
-q = Queue()
-q.enqueue("all ")
