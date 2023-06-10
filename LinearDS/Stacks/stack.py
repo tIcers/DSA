d6edab376c275936f28710a9f8f065795cfb9907
atsuki <csatsfapsfu2930@gmail.com>
2023-06-10 01:16:43 -0700

    implemented helper functions and test cases

--- LinearDS/Stacks/stack.py
+++ LinearDS/Stacks/stack.py
@@ -1,19 +1,57 @@
 from node import Node
 
 class Stack:
+  def __init__(self, limit=1000):
+    self.top_item = None
+    self.size = 0
+    self.limit = limit
+  
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+      print("Adding {} to the pizza stack!".format(value))
+    else:
+      print("No room for {}!".format(value))
 
-    def __init__(self, top_item=None):
-        self.top_item = top_item
+  def pop(self):
+    if not self.is_empty():
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      print("Delivering " + item_to_remove.get_value())
+      return item_to_remove.get_value()
+    print("All out of pizza.")
 
-    def push(self, value):
-        item = Node(value)
-        item.set_next_node(self.top_item)
-        self.top_item = item
+  def peek(self):
+    if not self.is_empty():
+      return self.top_item.get_value()
+    print("Nothing to see here!")
 
-    def pop(self):
-        item_to_remove = self.top_item
-        self.top_item = item_to_remove.get_next_node()
-        return item_to_remove.get_value()
+  def has_space(self):
+    return self.limit > self.size
 
-    def peek(self):
-        return self.top_item.get_value()
+  def is_empty(self):
+    return self.size == 0
+  
+pizza_stack = Stack(6)
+pizza_stack.push("pizza #1")
+pizza_stack.push("pizza #2")
+pizza_stack.push("pizza #3")
+pizza_stack.push("pizza #4")
+pizza_stack.push("pizza #5")
+pizza_stack.push("pizza #6")
+
+pizza_stack.push("pizza #7")
+
+print("The first pizza to deliver is " + pizza_stack.peek())
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+pizza_stack.pop()
+
+pizza_stack.pop()
