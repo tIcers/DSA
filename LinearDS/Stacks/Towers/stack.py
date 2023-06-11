1e5c02052faa583d52f4c55dc8ac3442c1cec234
atsuki <csatsfapsfu2930@gmail.com>
2023-06-11 12:56:50 -0700

    finished implementing stack

--- LinearDS/Stacks/Towers/stack.py
+++ LinearDS/Stacks/Towers/stack.py
@@ -19,34 +19,34 @@ class Stack:
 
     def pop(self):
         if self.size > 0:
-          item_to_remove = self.top_item
-          self.top_item = item_to_remove.get_next_node()
-          self.size -= 1
-          return item_to_remove.get_value()
+            item_to_remove = self.top_item
+            self.top_item = item_to_remove.get_next_node()
+            self.size -= 1
+            return item_to_remove.get_value()
         print("This stack is totally empty.")
 
-      def peek(self):
+    def peek(self):
         if self.size > 0:
           return self.top_item.get_value()
         print("Nothing to see here!")
 
-      def has_space(self):
+    def has_space(self):
         return self.limit > self.size
 
-      def is_empty(self):
+    def is_empty(self):
         return self.size == 0
       
-      def get_size(self):
+    def get_size(self):
         return self.size
       
-      def get_name(self):
+    def get_name(self):
         return self.name
       
-      def print_items(self):
+    def print_items(self):
         pointer = self.top_item
         print_list = []
         while(pointer):
-          print_list.append(pointer.get_value())
-          pointer = pointer.get_next_node()
+            print_list.append(pointer.get_value())
+            pointer = pointer.get_next_node()
         print_list.reverse()
         print("{0} Stack: {1}".format(self.get_name(), print_list))
