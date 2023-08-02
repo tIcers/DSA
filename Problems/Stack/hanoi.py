eed7b2b53d7aa26e8c39ff0b0b174921e3abbe0e
atsuki <csatsfapsfu2930@gmail.com>
2023-08-02 14:21:25 -0700

    implemented get_input method

--- Problems/Stack/hanoi.py
+++ Problems/Stack/hanoi.py
@@ -11,9 +11,8 @@ right_stack = Stack('Right')
 middle_stack = Stack('Middle')
 
 stack.append(left_stack)
-stack.append(right_stack)
 stack.append(middle_stack)
-
+stack.append(right_stack)
 
 # set up game
 
@@ -29,3 +28,22 @@ for i in range(num_disks, 0, -1):
 # the number of optimal moves is always 2 * number of disks  - 1
 num_optimal_moves = (2 ** num_disks)-1
 print('\nTHe fastest you can solve this game is in {0} moves'.format(num_optimal_moves))
+
+# Getting User input
+
+def get_input():
+
+    choices = [stack.get_name()[0] for stack in stacks]
+
+    while True:
+        for i in range(len(stacks)):
+            name = stacks[i].get_name()
+            letter = choices[i]
+            print(f'Enter {letter} for {name}')
+
+        user_input = input('')
+
+        if user_input in choices:
+            for i in range(len(stacks)):
+                if user_input == choices[i]:
+                    return stacks[i]
