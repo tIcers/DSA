3d485d78248939f7a3257736d680b568f938c04f
atsuki <csatsfapsfu2930@gmail.com>
2023-08-03 09:32:12 -0700

    implemented hash method

--- LinearDS/HashMap/script.py
+++ LinearDS/HashMap/script.py
@@ -2,3 +2,8 @@ class HashMap:
     def __init__(self, array_size):
         self.array_size = array_size
         self.array = [None for self.array in range(array_size)]
+
+    def hash(self, key):
+        key_bytes = key.encode()
+        hash_code = sum(key_bytes)
+        return hash_code
