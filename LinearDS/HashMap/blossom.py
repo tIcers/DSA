1362aa12efa67bf51bddeb909c67b7635a072a2d
atsuki <csatsfapsfu2930@gmail.com>
2023-08-04 10:37:52 -0700

    added in the liniked list

--- LinearDS/HashMap/blossom.py
+++ LinearDS/HashMap/blossom.py
@@ -1,7 +1,9 @@
+from linked_list import LinkedList
+
 class HashMap:
     def __init__(self, size):
         self.array_size = size
-        self.array = [None for i in range(array_size)]
+        self.array = [LinkedList() for i in range(array_size)]
 
     def hash(self, key):
         if isinstance(key, str):
@@ -24,5 +26,3 @@ class HashMap:
                 return payload[1]
         else:
             return None
-
-       
