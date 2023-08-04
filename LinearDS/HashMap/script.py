2447deed577df4278670568e976442b2bf8ac1b0
atsuki <csatsfapsfu2930@gmail.com>
2023-08-04 09:38:25 -0700

    dealt with open addressing in the getter

--- LinearDS/HashMap/script.py
+++ LinearDS/HashMap/script.py
@@ -20,11 +20,11 @@ class HashMap:
             new_array_index = self.compressor(new_hash_code)
             if self.array[new_array_index] is None:
                 self.array[new_array_index] = [key, value]
-                return 
+                return
             if self.array[new_array_index] == key:
                 self.array[new_array_index]  = [key, value]
-                return 
-            
+                return
+
             # collisions!
             number_collisions = 1
             while(current_array_value[0] != key):
@@ -38,17 +38,30 @@ class HashMap:
                 if current_array_value[0] == key:
                     self.array[array_index] = [key, value]
                     return
-            number_collisions +=1
+            number_collisions += 1
         return
 
     def retrieve(self, key):
         array_index = self.compressor(self.hash(key))
         possible_return_value = self.array[array_index]
+
         if possible_return_value is None:
             return None
         elif possible_return_value[0] == key:
             return possible_return_value[1]
 
+        retrieval_collisions = 1
+
+        while(possible_return_value != key):
+            new_hash_code = self.hash(key)
+            retrieving_array_index = self.compressor(new_hash_code)
+            possible_return_value = self.array[retrieving_array_index]
+
+            if possible_return_value is None:
+                return None
+            if possible_return_value[0] == key:
+                return possible_return_value[1]
+            retrieval_collisions += 1
 
 hash_map = HashMap(20)
 
