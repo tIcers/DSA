b00284f951e9428033f1d47a3820eeed253012b1
atsuki <csatsfapsfu2930@gmail.com>
2023-08-04 12:53:09 -0700

    finisedf implementing hashmap

--- LinearDS/HashMap/blossom.py
+++ LinearDS/HashMap/blossom.py
@@ -1,4 +1,5 @@
 from linked_list import LinkedList
+from blossom_lib import flower_definitions
 
 class HashMap:
     def __init__(self, size):
@@ -22,15 +23,28 @@ class HashMap:
         for item in list_at_array:
             if key == item[0]:
                 item[1] = value
+                return
             list_at_array.insert(payload)
-
-        
 
     def retrieve(self, key):
         array_index = self.compress(self.hash(key))
-        payload = self.array[array_index] 
+        list_at_index = self.array[array_index]
+        for item in list_at_index:
+            if key == item[0]:
+                return item[1] 
+            else:
+                return None
+
+            list_at_index.insert(list_at_index)
 
         if payload is not None and payload[0] == key:
                 return payload[1]
         else:
             return None
+
+blossom = HashMap(len(flower_definitions))
+
+for flower in flower_definitions:
+    blossom.assign(flower[0], flower[1])
+
+print(blossom.retrieve('daisy'))
