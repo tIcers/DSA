496d9522923972f3d74a9c391c2654561d5c038c
atsuki <csatsfapsfu2930@gmail.com>
2023-08-11 13:20:31 -0700

    simple sum recursion

--- LinearDS/AlgoConcepts/recursion.py
+++ LinearDS/AlgoConcepts/recursion.py
@@ -1,18 +1,6 @@
-class Recursion:
-    def __init__(self) -> None:
-        pass
-    def sum_to_one(n):
-        result = 1
-        call_stack = []
-
-        while n != 1:
-            execution_context = {"n_value": n}
-            call_stack.append(execution_context)
-            n -= 1
-            print(call_stack)
-        while len(call_stack) != 0:
-            return_value = call_stack.pop()
-            result += return_value["n_value"]
-        print("base case reached")
-
-        return result, call_stack
+def sum_to_one(n):
+    if n == 1:
+        return n
+    print("Recursing with input: {0}".format(n))
+    return n + sum_to_one(n - 1)
+print(sum_to_one(7))
