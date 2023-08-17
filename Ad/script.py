f720146a10bb3e673269abb5c6c3faf4c25ab452
atsuki <csatsfapsfu2930@gmail.com>
2023-08-16 17:32:41 -0700

    logging erros and msgs

--- Ad/script.py
+++ Ad/script.py
@@ -7,9 +7,28 @@ stream_handler = logging.StreamHandler(sys.stdout)
 
 logger.addHandler(stream_handler)
 
-print(logging.NOTSET)
-print(logging.DEBUG)
-print(logging.INFO)
-print(logging.WARNING)
-print(logging.ERROR)
-print(logging.CRITICAL)
+# print(logging.NOTSET)
+# print(logging.DEBUG)
+# print(logging.INFO)
+# print(logging.WARNING)
+# print(logging.ERROR)
+# print(logging.CRITICAL)
+
+def division():
+  try:
+    dividend = float(input("Enter the dividend: "))
+    logger.info(dividend)
+    divisor = float(input("Enter the divisor: "))
+    logger.info(divisor)
+  except ValueError:
+    logger.log(logging.CRITICAL, "No dividend or divisor value entered!")
+    return 
+  if divisor == 0:
+    logging.error("Attempting to divide by 0!")
+    return None
+  else:
+    return dividend/divisor
+result = division()
+
+if result == None:
+    logging.WARNING("The result value is None!")
