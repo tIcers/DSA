577f721c1112231c69b082f86b439fed1b306919
atsuki <csatsfapsfu2930@gmail.com>
2023-08-16 17:03:18 -0700

    logging level

--- Ad/script.py
+++ Ad/script.py
@@ -6,3 +6,10 @@ logger = logging.getLogger(__name__)
 stream_handler = logging.StreamHandler(sys.stdout)
 
 logger.addHandler(stream_handler)
+
+print(logging.NOTSET)
+print(logging.DEBUG)
+print(logging.INFO)
+print(logging.WARNING)
+print(logging.ERROR)
+print(logging.CRITICAL)
