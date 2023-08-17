be6b03239d4927296950046c855800377e484c83
atsuki <csatsfapsfu2930@gmail.com>
2023-08-17 10:20:27 -0700

    implemented sorting

--- LinearDS/AlgoConcepts/Asymptoic/findMax.py
+++ LinearDS/AlgoConcepts/Asymptoic/findMax.py
@@ -3,16 +3,25 @@ from linkedlist import LinkedList
 #Fill in Function
 def find_max(linked_list):
   print("--------------------------")
-  print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))   
+  print("Finding the maximum value of:\n{0}".format(linked_list.stringify_list()))
   current = linked_list.get_head_node()
-  maximum = current.value()
-  while current.get_next_node().value != None:
-        current = current.get_value()
-        if current > max_value:
-            max_value = current
-        return max_value
+  maximum = current.get_value()
+  while current.get_next_node():
+    current = current.get_next_node()
+    val = current.get_value()
+    if val > maximum:
+      maximum = val
+  return maximum
 
-    
+def sort_linked_list(linked_list):
+    print("\n---------------------------")
+    print("The original linked list is:\n{0}".format(linked_list.stringify_list()))
+    new_linked_list = LinkedList()
+    while linked_list.get_head_node():
+        current_max = find_max(linked_list)
+        linked_list.remove_node(current_max)
+        new_linked_list.insert_beginning(current_max)
+    return new_linked_list
 
 #Test Cases
 ll = LinkedList(6)
@@ -38,5 +47,5 @@ ll_3.insert_beginning("Q")
 print("The maximum value in this linked list is {0}\n".format(find_max(ll_3)))
 
 #Runtime
-runtime = "Replace"
+runtime = "N^2"
 print("The runtime of find_max is O({0})".format(runtime))
