94641a50d80dd45ba30811d1098e956621a0729e
atsuki <csatsfapsfu2930@gmail.com>
2023-12-24 07:03:17 -0800

    implemented Tree

--- Trees/script.py
+++ Trees/script.py
@@ -10,11 +10,15 @@ class TreeNode:
 
     def remove_child(self, child_node):
         print("Removing " + child_node.value + "from" + self.value)
-        new_children = []
-        for child in self.children:
-            if child != child_node:
-                new_children.append(child)
-            self.children = new_children
+        self.children = [self.children for child in self.children if child != child_node]
+
+    def traverse(self):
+        nodes_to_visit = [ self ]
+        while len( nodes_to_visit ) != 0:
+            current_node = nodes_to_visit.pop()
+            print(current_node.value)
+            nodes_to_visit += current_node.children
+
 
 root = TreeNode("I am Root")
 child = TreeNode("I am Child")
