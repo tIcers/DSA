eacffb2a7ed5e13643262e2b5b42ff4658a9041c
atsuki <csatsfapsfu2930@gmail.com>
2023-12-29 04:00:30 -0800

    implemented retrive_min method

--- Heap/min_heap.py
+++ Heap/min_heap.py
@@ -1,34 +1,47 @@
 class MinHeap:
-  def __init__(self):
-    self.heap_list = [None]
-    self.count = 0
+    def __init__(self):
+        self.heap_list = [None]
+        self.count = 0
 
-  def parent_idx(self, idx):
-    return idx // 2
+    def parent_idx(self, idx):
+        return idx // 2
 
-  def left_child_idx(self, idx):
-    return idx * 2
+    def left_child_idx(self, idx):
+        return idx * 2
 
-  def right_child_idx(self, idx):
-    return idx * 2 + 1
+    def right_child_idx(self, idx):
+        return idx * 2 + 1
 
 
-  def add(self, element):
-    self.count += 1
-    print("Adding: {0} to {1}".format(element, self.heap_list))
-    self.heap_list.append(element)
-    self.heapify_up()
-    
-  def heapify_up(self):
-    print("Heapifying up")
-    idx = self.count
-    while self.parent_idx(idx) > 0:
-        parent = self.heap_list[self.parent_idx(idx)]
-        child = self.heap_list[idx]
+    def add(self, element):
+        self.count += 1
+        print("Adding: {0} to {1}".format(element, self.heap_list))
+        self.heap_list.append(element)
+        self.heapify_up()
 
-        if parent > child:
-            print(f"swapping {parent} with {child}")
-            self.heap_list[idx] = parent
-            self.heap_list[self.parent_idx(idx)] = child
-        idx = self.parent_idx(idx)
-        print(f'Heap Restored {self.heap_list}')
+    def heapify_up(self):
+        print("Heapifying up")
+        idx = self.count
+        while self.parent_idx(idx) > 0:
+            parent = self.heap_list[self.parent_idx(idx)]
+            child = self.heap_list[idx]
+
+            if parent > child:
+                print(f"swapping {parent} with {child}")
+                self.heap_list[idx] = parent
+                self.heap_list[self.parent_idx(idx)] = child
+            idx = self.parent_idx(idx)
+            print(f'Heap Restored {self.heap_list}')
+
+    def retrieve_min(self):
+        if self.count == 0:
+            print("No items in heap")
+            return None 
+        min = self.heap_list[1]
+        print(f"Removing {min} from {self.heap_list}")
+
+        self.heap_list[1] = self.heap_list[self.count]
+        self.heap_list.pop()
+        self.count -= 1
+        print(f"Last element moved to first {self.heap_list}")
+        return min
