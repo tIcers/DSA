7a15cc376c5c2f59be6f12a05397dcad12ac5628
atsuki <csatsfapsfu2930@gmail.com>
2023-12-29 05:16:48 -0800

    implemented heapify_down method

--- Heap/min_heap.py
+++ Heap/min_heap.py
@@ -44,4 +44,24 @@ class MinHeap:
         self.heap_list.pop()
         self.count -= 1
         print(f"Last element moved to first {self.heap_list}")
+        self.heapify_down()
         return min
+
+    def heapify_down(self):
+        print("Heapifying down!")
+        idx = 1
+
+    def get_smaller_child_idx(self, idx):
+        if self.right_child_idx(idx) > self.count:
+            print("There is only a left child")
+            return self.left_child_idx(idx)
+        left_child = self.heap_list[self.left_child_idx(idx)]
+        right_child = self.heap_list[self.right_child_idx(idx)]
+
+        if left_child < right_child:
+            print('left child is smaller than right child')
+            return self.left_child_idx(idx)
+        else:
+            print('Right child is smaller')
+            return self.right_child_idx(idx)
+
---
+++ Heap/script.py
@@ -0,0 +1,16 @@
+# import random number generator
+from random import randrange
+# import heap class
+from min_heap import MinHeap 
+
+# make an instance of MinHeap
+min_heap = MinHeap()
+
+# populate min_heap with random numbers
+random_nums = [randrange(1, 101) for n in range(6)]
+for el in random_nums:
+  min_heap.add(el)
+
+
+# test it out, is the minimum number at index 1?
+print(min_heap.heap_list)
