b5d0e3565dc5fcaeb16ff1d79aec481bb1cd8931
atsuki <csatsfapsfu2930@gmail.com>
2023-12-31 16:45:01 -0800

    test merge sort

--- MergeSort/merge.py
+++ MergeSort/merge.py
@@ -4,7 +4,7 @@ def merge_sort(items):
     left_split = items[:middle_index]
     right_split = items[middle_index:]
     left_sorted, right_sorted = merge_sort(left_split) , merge_sort(right_split)
-    return merge(left_split, right_split)
+    return merge(left_sorted, right_sorted)
 
 def merge(left, right):
     result = []
@@ -21,3 +21,15 @@ def merge(left, right):
     if right:
         result += right 
     return result
+
+unordered_list1 = [356, 746, 264, 569, 949, 895, 125, 455]
+unordered_list2 = [787, 677, 391, 318, 543, 717, 180, 113, 795, 19, 202, 534, 201, 370, 276, 975, 403, 624, 770, 595, 571, 268, 373]
+unordered_list3 = [860, 380, 151, 585, 743, 542, 147, 820, 439, 865, 924, 387]
+
+ordered_list1 = merge_sort(unordered_list1)
+ordered_list2 = merge_sort(unordered_list2)
+ordered_list3 = merge_sort(unordered_list3)
+
+print('ordered_list1', ordered_list1)
+print('ordered_list2',ordered_list2)
+print('ordered_list3',ordered_list3)
