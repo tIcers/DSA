da7078155b00d2e17fdfa254e203c5f90abbe68a
atsuki <csatsfapsfu2930@gmail.com>
2024-01-02 11:25:11 -0800

    added edge

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -8,3 +8,9 @@ class Graph:
     def add_vertex(self, vertex):
         print("Adding " + vertex.value)
         self.graph_dict[vertex.value] = vertex
+
+    def add_edge(self, from_vertex, to_vertex):
+        print(f"Adding edge from {from_vertex.value} to {to_vertex.value}")
+        self.graph_dict[from_vertex.value].add_edge(to_vertex.value)
+        if not self.directed:
+            self.graph_dict[to_vertex.value].add_edge(from_vertex.value)
