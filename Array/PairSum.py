933da5fd546ccc64e296aed1a54e51d6379b4ada
atsuki <csatsfapsfu2930@gmail.com>
2024-01-13 13:05:10 -0800

    solved pairsum problem

--- Array/PairSum.py
+++ Array/PairSum.py
@@ -13,16 +13,13 @@
 #
 
 def pair_sum(arr, k):
-    if len(arr) < 2:
-        return 
     seen = set()
     output = set()
-
+    
     for num in arr:
         target = k - num
-
         if target not in seen:
-            seen.add(target)
+            seen.add(num)
         else:
-            output.add( (min(num, target), max(num, target)))
-    return len(output)
+            output.add(((min(num, target)), max(num, target)))
+    print('\n'.join(map(str, list(output))))
