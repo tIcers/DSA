e540439cb67f1cf2f1da71c385d35ba2d2adb50f
atsuki <csatsfapsfu2930@gmail.com>
2024-01-13 14:04:11 -0800

    solved pair_sum, and finder problems

---
+++ Array/Review.py
@@ -0,0 +1,44 @@
+# find pair sum for target k, and show the unique pair
+def pair_sum(arr, k):
+    seen = set()
+    output = set()
+    
+    for num in arr:
+        target = k - num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add(((min(num, target)), max(num, target)))
+    print('\n'.join(map(str, list(output))))
+
+# find missing number  from arr2 which is shuffled and compare with arr1
+def finder(arr1, arr2):
+    counter = {}
+
+    for num in arr1:
+        if num not in counter:
+            counter[num] = 1
+        else:
+            counter[num] += 1
+
+    for num in arr2:
+        if num not in counter:
+            counter[num] = 1
+        else:
+            counter[num] -= 1
+
+    for key, value in counter.items():
+        if value != 0:
+            return key
+
+# this is another solution using default dict
+
+import collections
+
+def finder2(arr1, arr2):
+    d = collections.defaultdict(int)
+    for num in arr2: d[num] +=1 
+
+    for num in arr1: 
+        if d[num] == 0: return num 
+        else: d[num] -= 1
