0e298116d696ab3e31e956d0bdee26c15be87902
atsuki <csatsfapsfu2930@gmail.com>
2024-01-13 18:23:00 -0800

    solved reversed word

--- Array/Review.py
+++ Array/Review.py
@@ -59,6 +59,41 @@ def large_count_sum(arr):
     for num in arr[1:]:
         current_sum = max(current_sum + num, num)
         max_sum = max(current_sum, max_sum)
-
     return max_sum
+
+# Sentence Reversal
+# Problem
+#
+# Given a string of words, reverse all the words. For example:
+#
+# Given:
+#
+# 'This is the best'
+#
+# Return:
+#
+# 'best the is This'
+#
+# As part of this exercise you should remove all leading and trailing whitespace. So that inputs such as:
+#
+# '  space here'  and 'space here      '
+#
+# both become:
+#
+# 'here space'
+
+
+def rev_word(s):
+    words = []
+    i = 0
+
+    while i < len(s):
+        if s[i] != ' ':
+            word_start = i
+            while i < len(s) and s[i] != ' ':
+                i += 1
+            words.append(s[word_start:i])
+        i += 1
+
+    return " ".join(reversed(words))
 
