a242d6ed687b2fead02cffb867d6ae3199948b0e
atsuki <csatsfapsfu2930@gmail.com>
2024-01-13 18:19:35 -0800

    large count sum

--- Array/Review.py
+++ Array/Review.py
@@ -53,3 +53,12 @@ def finder3(arr1, arr2):
 # Problem => Giver an array of intergers (postive and negative) find the largest countinous sum
 
 def large_count_sum(arr):
+    if len(arr) == 0:
+        return 0
+    max_sum = current_sum = 0
+    for num in arr[1:]:
+        current_sum = max(current_sum + num, num)
+        max_sum = max(current_sum, max_sum)
+
+    return max_sum
+
